90a368dba0b83f4013f5349331d8c01d8bac498c
prasad <ncprasadmadusanka@gmail.com>
2020-07-01 00:23:37 +0530

    Updates

---
+++ precache-manifest.3f6fdeec4d4243cd83f545de65a555cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c45ad5e32571326455d43a6d0a35a2c",
+    "url": "/03_React_JS_COVID-19_Tracker_Application_Demo/index.html"
+  },
+  {
+    "revision": "6b48b7fbb46f90520c2f",
+    "url": "/03_React_JS_COVID-19_Tracker_Application_Demo/static/css/main.cb690321.chunk.css"
+  },
+  {
+    "revision": "6498101f8830c60344f9",
+    "url": "/03_React_JS_COVID-19_Tracker_Application_Demo/static/js/2.e0db6ecf.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/03_React_JS_COVID-19_Tracker_Application_Demo/static/js/2.e0db6ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b48b7fbb46f90520c2f",
+    "url": "/03_React_JS_COVID-19_Tracker_Application_Demo/static/js/main.6a4bb843.chunk.js"
+  },
+  {
+    "revision": "a28ff548a302ad928a5d",
+    "url": "/03_React_JS_COVID-19_Tracker_Application_Demo/static/js/runtime-main.44e1cef4.js"
+  },
+  {
+    "revision": "c0d6cf84455c74d733d2e5a31a00063d",
+    "url": "/03_React_JS_COVID-19_Tracker_Application_Demo/static/media/covid_19_banner.c0d6cf84.jpg"
+  }
+]);
